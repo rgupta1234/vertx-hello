158dc63c173ebb3af1197b170b11d58d94beaf71
rgupta1234 <rkgupta75@gmail.com>
2020-08-09 12:08:07 -0500

    Update HelloServiceImpl.java

--- src/main/java/com/redhat/training/flozano/service/HelloServiceImpl.java
+++ src/main/java/com/redhat/training/flozano/service/HelloServiceImpl.java
@@ -3,9 +3,11 @@ package com.redhat.training.flozano.service;
 import io.vertx.core.AsyncResult;
 import io.vertx.core.Future;
 import io.vertx.core.Handler;
+import io.prometheus.client.Counter;
 
 public class HelloServiceImpl implements HelloService {
-
+static final Counter requests = Counter.build()
+     .name("requests_total").help("Total requests.").register();
 	@Override
 	public void hello(String host, String name, Handler<AsyncResult<String>> resultHandler) {
 		String msg = "RAVI GUPTA Hello " + name +  ", from '" + host + "'.\n";
